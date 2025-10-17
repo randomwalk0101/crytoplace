e2b0bc24574a6dc79a1bb07d6893df3fb078795e
randomwalk0101 <randomwalk0101@gmail.com>
2025-10-17 14:49:24 +0800

    20251017pm1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import{Routes,Route} from "react-router-dom";
+import Home from './pages/home/Home';
+import Coin from './pages/coin/Coin';
+
+
+
 
 const App = () => {
   return (
     <div className='app'> 
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/coin/:id' element={<Coin/>}/> 
+
+
+      </Routes>
 
 
 
