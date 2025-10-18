eed80923034ca0eb8a95fb980c93acc139d9ebcb
Your GitHub Username <randomwalk0101@gmail.com>
2025-10-18 18:17:13 +0800

    2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from './pages/home/Home';
 import Coin from './pages/coin/Coin';
 import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
+import { Footer } from './components/footer/Footer';
 
 
 
