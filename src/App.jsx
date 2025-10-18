3601f3b7e9633e31fa58a68b91cea508334f9be2
Your GitHub Username <randomwalk0101@gmail.com>
2025-10-18 18:02:42 +0800

    space

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Navbar from './components/navbar/Navbar'
-import{Routes,Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './pages/home/Home';
 import Coin from './pages/coin/Coin';
 
 
+import Navbar from './components/navbar/Navbar';
 import { Footer } from './components/footer/Footer';
 
 
