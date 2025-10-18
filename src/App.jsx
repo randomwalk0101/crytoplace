1be02664d2981c2ceacba1c167e6308595b734cb
Your GitHub Username <randomwalk0101@gmail.com>
2025-10-18 10:14:18 +0800

    Fix: Correct Navbar import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/Navbar/Navbar'
+import Navbar from './components/navbar/Navbar'
 import{Routes,Route} from "react-router-dom";
 import Home from './pages/home/Home';
 import Coin from './pages/coin/Coin';
