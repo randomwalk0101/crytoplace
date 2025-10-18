5f5c509a9b549b17f49d0ed14041263d5775ab23
Your GitHub Username <randomwalk0101@gmail.com>
2025-10-18 18:08:46 +0800

    big-small

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/home/Home';
 import Coin from './pages/coin/Coin';
-
-
 import Navbar from './components/navbar/Navbar';
+
 import { Footer } from './components/footer/Footer';
 
 
